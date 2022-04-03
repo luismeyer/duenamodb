cf5d2b1999469f2ac033e7a0144e959c326334ba
Luis Meyer <luis_meyer@outlook.de>
2022-04-03 15:05:42 +0200

    refactor: ci version bump

---
+++ scripts/update-version.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import prettier from 'prettier';
+
+import pjson from '../package.json';
+
+const { RELEASE_VERSION } = process.env;
+
+if (!RELEASE_VERSION) {
+  throw new Error("Couldn't find realese version in env variables");
+}
+
+pjson.version = RELEASE_VERSION.slice(1);
+
+const output = prettier.format(JSON.stringify(pjson), {
+  parser: 'json-stringify',
+});
+
+fs.writeFileSync(path.resolve(__dirname, '../package.json'), output);
